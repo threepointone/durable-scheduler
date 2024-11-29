d598e63610915efd4efef3da2e355fe898774b49
Sunil Pai <spai@cloudflare.com>
2024-11-29 12:57:27 +0000

    changesets

---
+++ .github/version-script.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+
+import { $ } from "bun";
+
+try {
+  console.log("Getting current git hash...");
+  const stdout = (await $`git rev-parse --short HEAD`).text();
+
+  for (const path of ["./package.json"]) {
+    const packageJson = JSON.parse(fs.readFileSync(path, "utf-8"));
+    packageJson.version = "0.0.0-" + stdout.trim();
+    fs.writeFileSync(path, JSON.stringify(packageJson, null, 2) + "\n");
+  }
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
